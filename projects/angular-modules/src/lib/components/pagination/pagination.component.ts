e23328c5b69c81c17683f8649b230ead17f4cc4a
Tudor Teisanu <tudor.teisanu@powerit.dev>
2023-04-27 14:23:36 +0300

    added angular modules lib

--- projects/angular-modules/src/lib/components/pagination/pagination.component.ts
+++ projects/angular-modules/src/lib/components/pagination/pagination.component.ts
@@ -1,6 +1,6 @@
 import {Component, Input} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {UtilsService} from '../../services/utils.service';
+import {UtilsService} from 'angular-modules';
 import {RouterLink} from '@angular/router';
 
 @Component({
